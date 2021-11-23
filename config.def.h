2035c2632af6735624a8a1bd6d3d22cf6afd4e07
Sebastian Andersen <sebastian@sandersen.co>
2021-11-23 09:37:07 +0100

    Just something about shutdown

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", colonoscopy, "-sf", col_gray4, "-g", "3", "-l", "2", NULL };
-static const char *shutdowncmd[]  = { "sudo", "shutdown", "+0", NULL };
+static const char *shutdowncmd[]  = { "sudo", "shutdown", "-h", "now",  NULL };
 
 static const char *termcmd[]  = { "st", NULL };
 
@@ -105,8 +105,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ControlMask|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask, XK_q,      quit,           {1} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {1} },
 	{ MODKEY|ShiftMask,             XK_Escape, spawn,          {.v = shutdowncmd} },
 };
 
